d61f06c23c8a53c4211d87b8b00790646fc6ee04
Mayur Badole <mayur.badole@ubiqc.com>
2024-07-28 19:41:36 +0530

    update code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 // src/redux/store.js
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
